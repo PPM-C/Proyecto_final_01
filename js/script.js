ff1d2fa27d7bf82299352cb4cb73eb3826413f29
Pamela Palacios <pamelapalacios@MacBook-Air-de-Pamela.local>
2025-07-09 01:01:07 +0200

    mensaje

---
+++ js/script.js
@@ -0,0 +1,7 @@
+ /*Menu hamburguesa */
+ const hamburger = document.querySelector('.hamburger');
+  const menuLinks = document.querySelector('.menu-links');
+
+  hamburger.addEventListener('click', () => {
+    menuLinks.classList.toggle('show');
+  });
